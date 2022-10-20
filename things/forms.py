53af8a8667134ac9362db58208fbe705e44e7d6e
Ismail Doghri <ismail.doghri@kcl.ac.uk>
2022-10-20 20:53:55 +0100

    Fix 'quantity' field

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django import forms
 from .models import Thing
 
@@ -5,7 +6,10 @@ from .models import Thing
 class ThingForm(forms.ModelForm):
     class Meta:
         model = Thing
-        fields = ['name', 'description']
+        fields = ['name', 'description', 'quantity']
         widgets = {'description': forms.Textarea() }
 
-        quantity = forms.CharField(label='Quantity', widget=forms.NumberInput())
+        quantity = forms.CharField(
+            widget=forms.NumberInput(),
+            validators = [MinValueValidator(0), MaxValueValidator(50)]
+        )
